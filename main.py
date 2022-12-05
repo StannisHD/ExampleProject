8636d6adf9045e5a3e61e3ce4f7e54d603b67cc2
StannisHD <hdixon@iupui.edu>
2022-12-04 21:45:54 -0500

    Pushing changes to remote (GitHub) repository.

---
+++ main.py
@@ -0,0 +1,22 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+def print_yo(name):
+    print(f'Greetings, (name)')
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+
+# if __name__ and __msg__ == '__main__':
+
+    print_hi('PyCharm')
+    print_hi('This is another line to show a change in code being pushed to GitHub')
+    print_yo('Adding a dummy file to this project to display it being pushed to GitHub')
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
